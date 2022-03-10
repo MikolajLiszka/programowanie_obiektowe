97e9917b2a63694570c1af4cb2c2084ef49fc2eb
MikolajLiszka <mikolaj.liszka@gmail.com>
2022-03-10 14:03:10 +0100

    tank2

--- lab_1/Program.cs
+++ lab_1/Program.cs
@@ -10,213 +10,214 @@ namespace lab_1
             Console.WriteLine(person.FirstName);*/
             //Money money = Money.Of("13,95", (Currency)1);
         }
-    }
 
-    public class Person
-    {
-        private string _firstName;
 
-        private Person(string firstName)
+        public class Person
         {
-            _firstName = firstName;
-        }
+            private string _firstName;
 
-        public static Person of(string firstName)
-        {
-            if (firstName.Length <= 2)
+            private Person(string firstName)
             {
-                return new Person(firstName);
+                _firstName = firstName;
             }
-            else
-            {
-                throw new ArgumentOutOfRangeException("Imię jest zbyt krótkie");
 
-            }
-        }
-
-        public string FirstName
-        {
-            get
+            public static Person of(string firstName)
             {
-                return _firstName;
-            }
-            set
-            {
-                if (value.Length >= 2)
+                if (firstName.Length <= 2)
                 {
-                    _firstName = value;
+                    return new Person(firstName);
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Imię jest zbyt krótkie");
+
+                }
+            }
+
+            public string FirstName
+            {
+                get
+                {
+                    return _firstName;
+                }
+                set
+                {
+                    if (value.Length >= 2)
+                    {
+                        _firstName = value;
+                    }
                 }
             }
         }
-    }
 
-    public enum Currency
-    {
-        PLN = 1,
-        USD = 2,
-        EUR = 3
-    }
-
-    public class Money
-    {
-        private readonly decimal _value;
-        private readonly Currency _currency;
-
-        public decimal Value
+        public enum Currency
         {
-            get { return _value; }
+            PLN = 1,
+            USD = 2,
+            EUR = 3
         }
 
-        public Currency Currency
+        public class Money
         {
-            get { return _currency; }
-        }
+            private readonly decimal _value;
+            private readonly Currency _currency;
 
-        private Money(decimal value, Currency currency)
-        {
-            _value = value;
-            _currency = currency;
-        }
-
-        public override string ToString()
-        {
-            return $"{_value} {_currency}";
-        }
-
-
-
-        public static Money Of(decimal value, Currency currency)
-        {
-            if (value < 0)
+            public decimal Value
             {
-                throw new Exception("Value should be biggerthan 0!");
+                get { return _value; }
             }
 
-            return new Money(value, currency);
-        }
-
-        public static Money ParseValue(string stringValue, Currency currency)
-        {
-            decimal value = decimal.Parse(stringValue);
-            return new Money(value, currency);
-        }
-
-        public static Money operator *(Money money, decimal factor)
-        {
-            return new Money(money.Value * factor, money.Currency);
-        }
-
-        public static Money operator +(Money a, Money b)
-        {
-            return new(a.Value + b.Value, a.Currency);
-        }
-
-        public static bool operator <(Money a, Money b)
-        {
-            return a.Value < b.Value;
-        }
-
-        public static bool operator >(Money a, Money b)
-        {
-            return a.Value > b.Value;
-        }
-
-        public static explicit operator double(Money money)
-        {
-            return (float)money.Value;
-        }
-
-    }
-
-    public class Tank
-    {
-        public readonly int Capacity;
-        private int _level;
-
-        public Tank(int capacity)
-        {
-            Capacity = capacity;
-        }
-
-        public override string ToString()
-        {
-            return $"Tank: Capacity = {Capacity}, Level = {_level}";
-        }
-
-        public int Level
-        {
-            get
+            public Currency Currency
             {
-                return _level;
+                get { return _currency; }
             }
 
-            private set
+            private Money(decimal value, Currency currency)
             {
-                if (value < 0 || value > Capacity)
+                _value = value;
+                _currency = currency;
+            }
+
+            public override string ToString()
+            {
+                return $"{_value} {_currency}";
+            }
+
+
+
+            public static Money Of(decimal value, Currency currency)
+            {
+                if (value < 0)
                 {
-                    throw new ArgumentOutOfRangeException();
+                    throw new Exception("Value should be biggerthan 0!");
                 }
 
-                _level = value;
+                return new Money(value, currency);
             }
-        }
 
-        /*public bool refuel(int amount)
-        {
-            if (amount < 0)
+            public static Money ParseValue(string stringValue, Currency currency)
             {
-                return false;
+                decimal value = decimal.Parse(stringValue);
+                return new Money(value, currency);
             }
-            if (_level + amount > Capacity)
-            {
-                return false;
-            }
-            _level += amount;
-            return true;
-        }*/
 
-        public bool consume(int amount)
-        {
-            if (amount < 0)
+            public static Money operator *(Money money, decimal factor)
             {
-                return false;
+                return new Money(money.Value * factor, money.Currency);
             }
-            if (_level + amount < Capacity)
-            {
-                return false;
-            }
-            Level = _level - amount;
-            return true;
 
-        }
+            public static Money operator +(Money a, Money b)
+            {
+                return new(a.Value + b.Value, a.Currency);
+            }
 
-        public void refuel(int amount)
-        {
-            if (amount < 0)
+            public static bool operator <(Money a, Money b)
             {
-                throw new ArgumentException("Argument cant be non positive!");
+                return a.Value < b.Value;
             }
-            if (_level + amount > Capacity)
-            {
-                throw new ArgumentException("Argument is too large!");
-            }
-            _level += amount;
-        }
 
-        /*public bool refuel(Tank sourcetank, int amount)
-        {
-            if (amount < 0)
+            public static bool operator >(Money a, Money b)
             {
-                return false;
+                return a.Value > b.Value;
             }
-            if (_level + amount > Capacity)
+
+            public static explicit operator double(Money money)
             {
-                return false;
-            }    
+                return (float)money.Value;
+            }
 
         }
-        */
-        
+
+        public class Tank
+        {
+            public readonly int Capacity;
+            private int _level;
+
+            public Tank(int capacity)
+            {
+                Capacity = capacity;
+            }
+
+            public override string ToString()
+            {
+                return $"Tank: Capacity = {Capacity}, Level = {_level}";
+            }
+
+            public int Level
+            {
+                get
+                {
+                    return _level;
+                }
+
+                private set
+                {
+                    if (value < 0 || value > Capacity)
+                    {
+                        throw new ArgumentOutOfRangeException();
+                    }
+
+                    _level = value;
+                }
+            }
+
+            /*public bool refuel(int amount)
+            {
+                if (amount < 0)
+                {
+                    return false;
+                }
+                if (_level + amount > Capacity)
+                {
+                    return false;
+                }
+                _level += amount;
+                return true;
+            }*/
+
+            public bool consume(int amount)
+            {
+                if (amount < 0)
+                {
+                    return false;
+                }
+                if (_level + amount < Capacity)
+                {
+                    return false;
+                }
+                Level = _level - amount;
+                return true;
+
+            }
+
+            public void refuel(int amount)
+            {
+                if (amount < 0)
+                {
+                    throw new ArgumentException("Argument cant be non positive!");
+                }
+                if (_level + amount > Capacity)
+                {
+                    throw new ArgumentException("Argument is too large!");
+                }
+                _level += amount;
+            }
+
+            /*public bool refuel(Tank sourcetank, int amount)
+            {
+                if (amount < 0)
+                {
+                    return false;
+                }
+                if (_level + amount > Capacity)
+                {
+                    return false;
+                }    
+
+            }
+            */
+
+        }
     }
 }
 
