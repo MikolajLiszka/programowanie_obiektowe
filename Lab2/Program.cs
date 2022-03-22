9f5ddd70b160df4de57e2a303cc83da1c0f301d4
MikolajLiszka <mikolaj.liszka@gmail.com>
2022-03-22 23:13:28 +0100

    ćwiczenie 1 lab 2

---
+++ Lab2/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+
+namespace lab_2_na_zajeciach
+{
+    abstract class AbstractMessage
+    {
+        public string Content { get; init; }
+        public abstract bool Send();
+    }
+
+    public interface IFly
+    {
+        void Fly();
+    }
+
+    public interface ISwim
+    {
+        void Swim();
+    }
+
+    class Duck : IFly, ISwim
+    {
+        public void Fly()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Swim()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Hydroplane : IFly, ISwim
+    {
+        public void Fly()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Swim()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class EmailMessage : AbstractMessage
+    {
+        public string To { get; init; }
+        public string From { get; init; }
+        public string Subject { get; init; }
+        public override bool Send()
+        {
+            Console.WriteLine($"Sending email from: {From} to {To} with content {Content}");
+            return true;
+        }
+    }
+
+    class SmsMessage : AbstractMessage
+    {
+        public string ToPhone { get; init; }
+        public string FromPhone { get; init; }
+        public override bool Send()
+        {
+            Console.WriteLine($"Sending sms from: {FromPhone} to {ToPhone} with content {Content}");
+            return true;
+        }
+    }
+
+    class MessengerMessage : AbstractMessage
+    {
+        public override bool Send()
+        {
+            Console.WriteLine($"Sending messenger message with content{Content}");
+            return true;
+        }
+    }
+
+    class User
+    {
+        public string Name { get; init; }
+        public AbstractMessage LastMessage { get; init; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string messageType = "SMS";
+            switch (messageType)
+            {
+                case "SMS":
+                    Console.WriteLine("Sending SMS");
+                    break;
+                case "email":
+                    Console.WriteLine("Sending SMS");
+                    break;
+            }
+            User[] users =
+            {
+                new User() {Name="Karol", LastMessage = new SmsMessage(){Content="Hello", FromPhone="0949049" , ToPhone="55476756"}
+                },
+                new User() {Name="Karol", LastMessage = new EmailMessage(){Content="Hello", From="jdjdjd" , To="jdjdjdjdj"}
+                }
+            };
+
+
+            int EmailCounter = 0;
+            foreach (var user in users)
+            {
+                user.LastMessage.Send();
+                if (user.LastMessage is EmailMessage)
+                {
+                    EmailCounter++;
+                }
+                if (user.LastMessage is SmsMessage)
+                {
+                    SmsMessage sms = (SmsMessage)user.LastMessage;
+                    Console.WriteLine(sms.ToPhone);
+                }
+            }
+            Console.WriteLine($"Wysłano wiadomości email: {EmailCounter}");
+
+            IFly[] flyingObject =
+            {
+                new Duck(),
+                new Hydroplane()
+            };
+        }
+    }
+    interface IAggragate
+    {
+        IIterator createIterator();
+    }
+
+    interface IIterator
+    {
+        bool HasNext();
+        int GetNext();
+
+    }
+
+    class SimpleAggregate : IAggragate
+    {
+        public int a = 5;
+        public int b = 9;
+        public int c = 6;
+        public IIterator createIterator()
+        {
+            return new SimpleAggregateIterator(this);
+        }
+    }
+
+    class SimpleAggregateIterator : IIterator
+    {
+        private SimpleAggregate _aggregate;
+        private int count = 0;
+
+        public SimpleAggregateIterator(SimpleAggregate aggregate)
+        {
+            _aggregate = aggregate;
+        }
+
+        public int GetNext()
+        {
+            switch (++count)
+            {
+                case 1:
+                    return _aggregate.a;
+                case 2:
+                    return _aggregate.b;
+                case 3:
+                    return _aggregate.c;
+            }
+        }
+
+        public bool HasNext()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ lab_2_cw_od_1_do_3/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+
+namespace lab_2_cwiczenie_1
+{
+    public abstract class Vehicle
+    {
+        public double Weight { get; init; }
+        public int MaxSpeed { get; init; }
+        protected int _mileage;
+        public int Mileage
+        {
+            get { return _mileage; }
+        }
+        public abstract decimal Drive(int distance);
+        public override string ToString()
+        {
+            return $"Vehicle{{ Weight: {Weight}, MaxSpeed: {MaxSpeed}, Mileage: {_mileage} }}";
+        }
+    }
+
+    public class Scooter : Vehicle
+    {
+        public bool isDriver { get; set; }
+        public override decimal Drive(int distance)
+        {
+            if (isDriver)
+            {
+                _mileage += distance;
+                return (decimal)(distance / (double)MaxSpeed);
+            }
+            return -1;
+        }
+        public override string ToString()
+        {
+            return $"Scooter{{Weight: {Weight}, MaxSpeed: {MaxSpeed}, Mileage: {_mileage}}}"; ;
+        }
+
+    }
+
+    public class ElectricScooter: Scooter
+    {
+        public int BatteriesLevel { get; init; }
+        public int MaxRange { get; init; }
+
+        public void ChargeBatteries()
+        {
+            if (BatteriesLevel < 100)
+            {
+                for (int i = BatteriesLevel; i <= 101; i++)
+                {
+                    Console.WriteLine("Bateria Naładowana");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Bateria w pełni naładowana");
+            }
+        }
+        public override decimal Drive(int distance)
+        {
+            if (BatteriesLevel > 0)
+            {
+                _mileage += distance;
+                return (decimal)(distance / (double)MaxSpeed);
+            }
+            return -1;
+        }
+    }
+
+    public class KickScooter : Scooter
+    {
+        public override decimal Drive(int distance)
+        {
+            if (isDriver)
+            {
+                _mileage += distance;
+                return (decimal)(distance / (double)MaxSpeed);
+            }
+            return -1;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle[] vehicles =
+            {
+                new Scooter(){Weight = 15, MaxSpeed = 35, isDriver = true},
+                new ElectricScooter(){Weight = 88, MaxSpeed = 80, isDriver = true, BatteriesLevel = 88},
+                new ElectricScooter(){Weight = 88, MaxSpeed = 80, isDriver = true, BatteriesLevel = 88},
+                new ElectricScooter(){Weight = 88, MaxSpeed = 80, isDriver = true, BatteriesLevel = 88},
+                new ElectricScooter(){Weight = 88, MaxSpeed = 80, isDriver = true, BatteriesLevel = 88},
+                new ElectricScooter(){Weight = 88, MaxSpeed = 80, isDriver = true, BatteriesLevel = 88},
+                new ElectricScooter(){Weight = 88, MaxSpeed = 80, isDriver = true, BatteriesLevel = 33},
+                new ElectricScooter(){Weight = 88, MaxSpeed = 80, isDriver = true, BatteriesLevel = 88},
+                new ElectricScooter(){Weight = 88, MaxSpeed = 80, isDriver = true, BatteriesLevel = 88},
+                new ElectricScooter(){Weight = 88, MaxSpeed = 80, isDriver = true, BatteriesLevel = 0},
+                new KickScooter(){Weight = 88, MaxSpeed = 80, isDriver = true}
+            };
+
+            foreach (var vehicle in vehicles)
+            {
+                Console.WriteLine("Time for distance: " + vehicle.Drive(100));
+            }
+
+            int scooterCounter = 0;
+            int electricScooterCounter = 0;
+            int kickScooterCounter = 0;
+
+            foreach (var vehicle in vehicles)
+            {
+                if (vehicle is Scooter)
+                {
+                    scooterCounter++;
+                    Scooter scooter = (Scooter)vehicle;
+                    Console.WriteLine("Czy skuter ma kierowcę: " + scooter.isDriver);
+                }
+                if (vehicle is ElectricScooter)
+                {
+                    electricScooterCounter++;
+                }
+                if (vehicle is Scooter)
+                {
+                    kickScooterCounter++;
+                }
+            }
+            Console.WriteLine($"Liczba skuterów: {scooterCounter}, liczba samochodów: {electricScooterCounter}");
+        }
+    }
+}
